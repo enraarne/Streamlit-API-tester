a952d81fc0af0997ba4beaf4ca65fac568d9e509
Arne Martin Fevolden <Arne.Martin.Fevolden@udir.no>
2023-10-19 14:03:56 +0200

    ensured valid org nummer for get_orgnummer

--- API_helper_functions/load_data.py
+++ API_helper_functions/load_data.py
@@ -4,14 +4,39 @@ import csv
 import pandas as pd
 import requests
 
+from API_helper_functions.get_tidID import trines_get_tidID
+
+
 # Laster inn orgnummer
 @st.cache_data(max_entries=10)
-def get_orgnummer(url = "https://api.statistikkbanken.udir.no/api/rest/v2/Eksport/148/filterVerdier"):
+def get_orgnummer():
+    """
+    Den første spørringen henter alle orgnummer i den valgte tabellen. Den andre henter gyldige enhetIDer for et
+    spesifikt år. Deretter filtreres orgnummerne mot gyldige enhetIDer.
+    """
+    
+    TidID     = trines_get_tidID()
+    
+    url = "https://api.statistikkbanken.udir.no/api/rest/v2/Eksport/148/filterVerdier"
     response = requests.get(url)
     orgnummer = []
     for enhet in response.json()['EnhetID']:
-        orgnummer.append(enhet['kode'])
-    return orgnummer
+        orgnummer.append(( enhet['id'], enhet['kode']))
+    
+    
+    url = f"https://api.statistikkbanken.udir.no/api/rest/v2/Eksport/148/filterStatus?filterId=EnhetID&filtre=TidID({TidID})"
+    response = requests.get(url)
+    enhetIDer = []
+    for enhet in response.json()['EnhetID']:
+        enhetIDer.append(enhet)
+        
+    gyldige_enheter = []
+    
+    for item in orgnummer:
+        if item[0] in enhetIDer:
+            gyldige_enheter.append(item[1])
+        
+    return gyldige_enheter
 
 # Laster inn fylkesnummere
 @st.cache_data(max_entries=100)
